6c22e097e7161b4b835e919a0a618ee4c0c937fe
Daniel Große <hallo@daniel-der-grosse.de>
2021-05-02 10:22:11 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,22 @@
-input.calibrateCompass()
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    richtung = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    richtung = 0
+})
+input.onButtonPressed(Button.B, function () {
+    richtung = -1
+})
+let richtung = 0
+basic.showIcon(IconNames.Heart)
+let strip = neopixel.create(DigitalPin.P8, 12, NeoPixelMode.RGB)
+let range = strip.range(0, 12)
+range.showRainbow(1, 256)
 basic.forever(function () {
-    basic.clearScreen()
-    if (input.compassHeading() >= 337.5 || input.compassHeading() <= 22.5) {
-        basic.showArrow(ArrowNames.North)
-    } else if (input.compassHeading() >= 22.5 || input.compassHeading() <= 67.5) {
-        basic.showArrow(ArrowNames.NorthEast)
-    } else if (input.compassHeading() >= 67.5 || input.compassHeading() <= 113.5) {
-        basic.showArrow(ArrowNames.East)
-    } else if (input.compassHeading() >= 113.5 || input.compassHeading() <= 158.5) {
-        basic.showArrow(ArrowNames.SouthEast)
-    } else if (input.compassHeading() >= 158.5 || input.compassHeading() <= 203.5) {
-        basic.showArrow(ArrowNames.South)
-    } else if (input.compassHeading() >= 203.5 || input.compassHeading() <= 248.5) {
-        basic.showArrow(ArrowNames.SouthWest)
-    } else if (input.compassHeading() >= 248.5 || input.compassHeading() <= 293.5) {
-        basic.showArrow(ArrowNames.SouthWest)
-    } else if (input.compassHeading() >= 293.5 || input.compassHeading() <= 338.5) {
-        basic.showArrow(ArrowNames.West)
-    } else if (input.compassHeading() >= 293.5 || input.compassHeading() <= 338.5) {
-        basic.showArrow(ArrowNames.NorthWest)
-    } else {
-        basic.clearScreen()
-    }
+    strip.rotate(richtung)
+    strip.show()
+    basic.pause(100)
 })
